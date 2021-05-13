cd02469e3a0cda43c730f51d0f122da84a52facf
mandrent <atueid@gmail.com>
2021-05-13 17:35:24 -0300

    Ajusta o AreaTipo
    Remove a constante ZERO do AreaTipo

--- src/main/java/io/oneo/agon/modules/profissional/type/AreaTipo.java
+++ src/main/java/io/oneo/agon/modules/profissional/type/AreaTipo.java
@@ -2,7 +2,6 @@ package io.oneo.agon.modules.profissional.type;
 
 public enum AreaTipo
 {
-    ZERO,
     ENGENHARIA_SEGURANCA,
     MEDICIANA_OCUPACIONAL;
 }
--- src/main/java/io/oneo/agon/modules/usuario/type/NivelTipo.java
+++
@@ -1,6 +0,0 @@
-package io.oneo.agon.modules.usuario.type;
-
-public enum NivelTipo
-{
-    ZERO, ADMIN, SIMPLES, PADRAO, PERSONALIZADO, PREMIER;
-}
