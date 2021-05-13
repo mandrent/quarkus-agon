cf1c910bb2a79d15284d65343805474f74e2ce22
mandrent <atueid@gmail.com>
2021-05-13 17:41:33 -0300

    Ajusta as interfaces ITelefoneService e IUsuarioService
    Ajusta as interfaces ITelefoneService e IUsuarioService

--- src/main/java/io/oneo/agon/modules/telefone/service/ITelefoneService.java
+++ src/main/java/io/oneo/agon/modules/telefone/service/ITelefoneService.java
@@ -2,5 +2,5 @@ package io.oneo.agon.modules.telefone.service;
 
 public interface ITelefoneService
 {
-
+    String validarFormatoNumero(String numero);
 }
--- src/main/java/io/oneo/agon/modules/usuario/service/IUsuarioService.java
+++ src/main/java/io/oneo/agon/modules/usuario/service/IUsuarioService.java
@@ -1,6 +1,19 @@
 package io.oneo.agon.modules.usuario.service;
 
+import io.oneo.agon.modules.usuario.exception.UsuarioServiceException;
+import io.oneo.agon.modules.usuario.model.Usuario;
+import io.oneo.agon.modules.usuario.type.GrupoTipo;
+import io.oneo.agon.modules.usuario.type.StatusUsuarioTipo;
+
+import java.util.Optional;
+
 public interface IUsuarioService
 {
+    GrupoTipo validarGrupo(Integer grupoValor);
 
+    StatusUsuarioTipo validarStatus(Integer statusValor);
+
+    Optional<Usuario> findByLogin(String login) throws UsuarioServiceException;
+
+    Optional<Usuario> findByEmail(String email) throws UsuarioServiceException;
 }
