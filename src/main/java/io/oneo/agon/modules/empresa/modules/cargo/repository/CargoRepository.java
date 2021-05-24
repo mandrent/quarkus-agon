f2659f87f8499057f46ae35caacd36149a69eb72
mandrent <atueid@gmail.com>
2021-05-23 23:26:25 -0300

    Altera o retorno das consultas no CargoRepository
    Altera o retorno das consultas no CargoRepository

--- src/main/java/io/oneo/agon/modules/empresa/modules/cargo/repository/CargoRepository.java
+++ src/main/java/io/oneo/agon/modules/empresa/modules/cargo/repository/CargoRepository.java
@@ -1,6 +1,5 @@
 package io.oneo.agon.modules.empresa.modules.cargo.repository;
 
-
 import io.oneo.agon.modules.empresa.modules.cargo.model.Cargo;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Parameters;
@@ -16,22 +15,25 @@ public class CargoRepository implements PanacheRepository<Cargo>
         return this.find("nome", nome).firstResultOptional();
     }
 
-    public Optional<Cargo> findByFunction(String setor)
+    public Optional<Cargo> findByFunction(String funcao) { return this.find("funcao", funcao).firstResultOptional(); }
+
+    public Optional<Cargo> findBySector(String setor) { return this.find("setor", setor).firstResultOptional(); }
+
+    public Optional<Cargo> findByReference(String referencia)
     {
-        return this.find("setor", setor).firstResultOptional();
+        return this.find("referencia", referencia).firstResultOptional();
     }
 
-    public Optional<Cargo> findBydesc(String desc)
+    public Optional<Cargo> findByDescription(String description)
     {
-        return this.find("desc", desc).firstResultOptional();
+        return this.find("description", description).firstResultOptional();
     }
 
-    public Optional<Cargo> findByNameSectorFunction(String name, String sector, String function)
+    public Optional<Cargo> findByNameSectorFunction(String nome, String setor, String funcao)
     {
-        return this.find("name = :name and sector = :sector and function = :function",
-                Parameters.with("name", name)
-                        .and("sector", sector)
-                        .and("function", function)).firstResultOptional();
-
+        return this.find("nome = :nome and setor = :setor and funcao = :funcao",
+                Parameters.with("nome", nome)
+                        .and("setor", setor)
+                        .and("funcao", funcao)).firstResultOptional();
     }
 }
