a5395d9eb5532bf034ea85d975142db4c4956272
mandrent <atueid@gmail.com>
2021-06-21 14:15:50 -0300

    Altera a definição do RestClient no application.yaml
    Altera a definição do RestClient no application.yaml

--- src/main/java/io/oneo/agon/modules/telefone/resource/dto/TelefoneDTO.java
+++ src/main/java/io/oneo/agon/modules/telefone/resource/dto/TelefoneDTO.java
@@ -3,11 +3,13 @@ package io.oneo.agon.modules.telefone.resource.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.oneo.agon.modules.telefone.type.TelefoneTipo;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TelefoneDTO implements Serializable
 {
+    @Serial
     private static final long serialVersionUID = -782188649105570983L;
 
     public Long id;
